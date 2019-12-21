60683f0d36096f7a43e482bd509da51003ed9832
anujpuri72 <anujpuri72@gmail.com>
2019-12-21 13:40:17 +0530

    implemented approval Card

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import faker from "faker";
+
+const Card = props => {
+    return ( 
+<div className="ui cards">
+    <div className="card">
+      <div className="content">
+            {props.children}
+      </div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Approve</div>
+          <div className="ui basic red button">Decline</div>
+        </div>
+      </div>
+    </div>
+</div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDom from "react-dom";
 import faker from "faker";
 import CommentDetail from './CommentDetail'
+import Card from './Card'
 const App = () => {
   return (
+    
     <div className="ui container comments">
+    <Card>
       <CommentDetail author ="Pushkar" timeago="2:00 PM" textwithin =" hola Bhaiyooon" />
+      </Card>
+      <Card>
       <CommentDetail author ="Anuj" timeago="2:01 PM" textwithin =" kaisn hai sab" />
-      <CommentDetail author ="Prakash" timeago="2:02 PM" textwithin =" tumahri yaaad me" />
-
-     
-    </div>
+      </Card>
+      <Card>
+      <CommentDetail author ="Prakash" timeago="2:02 PM" textwithin =" tumahri yaaad me " />     
+      </Card>
+      </div>
   );
 };
 ReactDom.render(<App />, document.querySelector("#root"));
