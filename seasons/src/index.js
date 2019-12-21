8d9f0084737fef62c3213228cc41f00b7a4ea9d1
anujpuri72 <anujpuri72@gmail.com>
2019-12-21 14:24:31 +0530

    new project learning apis

---
+++ seasons/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err=>console.log(err)
+    );
+  return (
+    <div>
+    Hi there
+      </div>
+  );
+};
+ReactDom.render(<App />, document.querySelector("#root"));
