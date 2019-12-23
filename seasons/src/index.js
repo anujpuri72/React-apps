609cb71cc36fd46c348fb620a77e3673e8df572d
anujpuri72 <anujpuri72@gmail.com>
2019-12-23 13:41:43 +0530

    changing to class based cmponents

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => console.log(position),
+//     err => console.log(err)
+//   );
+//   return <div> Hi there </div>;
+// };
+class App extends React.Component {
+  render() {
     window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err=>console.log(err)
+      position => console.log(position),
+      err => console.log(err)
     );
-  return (
-    <div>
-    Hi there
-      </div>
-  );
-};
+    return <div> Latitude </div>;
+  }
+}
+
 ReactDom.render(<App />, document.querySelector("#root"));
