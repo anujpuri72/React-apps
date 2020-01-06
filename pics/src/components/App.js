a00a11ace33246d0a9cdc985b7c4fc960614f196
anujpuri72 <anujpuri72@gmail.com>
2020-01-06 17:03:08 +0530

    pics api needed

---
+++ pics/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onsubmitted={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  //   onInputChange(event) {
+  //     console.log(event.target.value);
+  //   }
+  state = { term: "" };
+  onFormSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.term);
+    this.props.onsubmitted(this.state.term);
+  };
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Iamge SearchBar</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e =>
+                this.setState({
+                  term: e.target.value
+                })
+              }
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
