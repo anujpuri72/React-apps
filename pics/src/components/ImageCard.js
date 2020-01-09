6df3c99dffe9e5dab2605b9f7236e7ff789d8521
anujpuri72 <anujpuri72@gmail.com>
2020-01-09 14:31:12 +0530

    working

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ImageRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.ImageRef);
+    console.log(this.ImageRef.current.clientHeight);
+  }
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.ImageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
